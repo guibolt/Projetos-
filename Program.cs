a29942f9c9aec06f403d9de0c527a60b18f1a186
guibolt <guilhermedosreiss@gmail.com>
2019-06-18 23:51:29 -0300

    bora pro push

--- Program.cs
+++ Program.cs
@@ -1,85 +1,82 @@
 ﻿using System;
-
+using System.Globalization;
 namespace Avaliacao
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string[] vetNomes = new string[5];
+            double[] vetNotas = new double[5];
+            double  nota=0, contador = 0;
+            int porc,aprovados =0,reprovados=0;
+            char tecla = 's';
 
-
-            // nomes dos alunos 
-
-            //string aluno1, aluno2, aluno3, aluno4, aluno5;
-            //int notas1,nota2,nota3,nota4,nota5;
-
-            //string[] vetNomes;
-
-            //vetNomes = new string[4];
-
-
-             int  media;
-             int[] vetNotas;
-
-
-            int contador = 0;
-
-             string[] s;
-
-             vetNomes = new string[5]; 
-             vetNotas = new int[5];
-            contNomes = new string[5];
-         
-            // Nome dos Alunos
-            Console.WriteLine("Digite os nomes  dos  Alunos de");
-
-
-            for(int i = 0; i < 5; i++)
+            do
             {
-
-               vetNomes[i] = Convert.ToString(Console.ReadLine());
-               contNomes[i] = Convert.ToString(vetNotas[i]);
-
-                
-
-            }
-            //  Notas dos allunos 
-             Console.WriteLine("Digite as notas  0 a 10");
-
-            
-
-            for ( int p = 0; p<5; p++)
-            {
-                
-                 vetNotas[p] =int.Parse(Console.ReadLine());
-
-                if (vetNotas[p] > 5)
+                for (int i = 0; i < 5; i++)
                 {
-                    for (int l = 0; l < 5; l++)
+              
+                    try
                     {
-                        if (vetNotas[l] > 5)
-                        {
-                            Console.WriteLine(vetNomes[l]);
+                        // Nome dos Alunos
+                        Console.WriteLine("Digite o nome do aluno: ");
+                        string nome = Convert.ToString(Console.ReadLine());
+                        vetNomes[i] = nome;
 
-                            Console.WriteLine(vetNotas[p] + "Aprovado");
+                        Console.WriteLine("Digite a nota do aluno");
+                        nota = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                        vetNotas[i] = nota;
+
+                        contador += vetNotas[i];
+
+                        while (nota > 10 || nota < 0)
+                        {
+                            Console.WriteLine("Nota Errada");
+                            Console.WriteLine("Digite uma nota certa");
+                            nota = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
                         }
-                        
+
+                        if (vetNotas[i] >= 5)
+                        {
+                            Console.WriteLine("Voce foi aprovado " + vetNomes[i] + " Com a nota de: " + vetNotas[i]);
+                            aprovados++;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Voce foi reprovado " + vetNomes[i] + " Com a nota de: " + vetNotas[i]);
+                            reprovados++;
+                        }
+                    }
+
+                    catch (Exception)
+                    {
+
+                        Console.WriteLine("Erro, insira um valor compativel.");
+                        i--;
 
                     }
+
                 }
-              
 
+                //   Calculo das medias
+                Console.WriteLine("A media da sala foi: " + contador / 5);
 
-                contador = contador + vetNotas[p];
+                // Calculo da porcentagem
+                porc = aprovados * 20;
+                Console.WriteLine("A % de Alunos aprovados é: " + porc);
 
-             }
-            // Calculo das medias
-                 media = contador / 5;
-                Console.WriteLine(media);
+                porc = reprovados * 20;
+                Console.WriteLine("A % de Alunos reprovados é: " + porc);
 
+                Console.WriteLine("Deseja calcular novamente ? s para sim e n para não!");
 
-
-
+                tecla = char.Parse(Console.ReadLine());
+                Console.Clear();
+                
+            } while (tecla == 's');
 
         }
     }
